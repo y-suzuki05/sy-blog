4b01a695843feff1330fe1c9f16de7bd3fce0c9b
y-suzuki05 <momo_968_weekend@yahoo.co.jp>
2023-10-09 14:36:40 +0900

    headerコンポーネント仮実装

--- src/components/navigation/header/header.tsx
+++ src/components/navigation/header/header.tsx
@@ -1,16 +1,27 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
+import Link from "next/link";
 
 export const Header = () => {
   return (
-    <Box as={"header"} h={"3.875rem"} bg={"white"} position={'fixed'} w={'100%'} zIndex={'sticky'}>
+    <Box
+      as={"header"}
+      h={"3.875rem"}
+      bg={"white"}
+      position={"fixed"}
+      w={"100%"}
+      zIndex={"sticky"}
+    >
       <Flex
         justifyContent={"space-between"}
         color={{ base: "blue", md: "red" }}
         w={{ base: "100%", md: "70rem" }}
         m={"auto"}
-        h={'100%'}
+        h={"100%"}
+        alignItems={"center"}
       >
-        テストブログ
+        <Heading as={"h1"} color={"#3ea8ff"}>
+          <Link href={"/"}>テストブログ</Link>
+        </Heading>
       </Flex>
     </Box>
   );
