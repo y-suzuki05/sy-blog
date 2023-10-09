eed21419fa1b05abc5cc40c923e59827affc7ef6
y-suzuki05 <momo_968_weekend@yahoo.co.jp>
2023-10-09 14:26:08 +0900

    記事一覧の横並び対応

--- src/components/top/top.tsx
+++ src/components/top/top.tsx
@@ -15,36 +15,36 @@ export const Top = ({ blogData }: TopProps) => {
       <Heading as={"h2"} mt={"3.5rem"} mb={"1.125rem"}>
         記事一覧
       </Heading>
-      <HStack spacing={30} wrap={"wrap"}>
+      <Flex wrap={"wrap"} gap={4}>
         {blogData.contents.map((content) => {
           return (
-            <Box key={content.id}>
-              <Flex gap={8}>
+            <Flex key={content.id} gap={8} w={{ base: "100%", md: "45%" }}>
+              <Link href={`/blog/${content.id}`}>
+                <Image
+                  src={content.eyecatch.url}
+                  width={90}
+                  height={90}
+                  alt="アイキャッチ"
+                />
+              </Link>
+              <Box>
                 <Link href={`/blog/${content.id}`}>
-                  <Image
-                    src={content.eyecatch.url}
-                    width={90}
-                    height={90}
-                    alt="アイキャッチ"
-                  />
+                  <Text fontSize={16} fontWeight={700}>
+                    {content.title}
+                  </Text>
                 </Link>
-                <Box>
-                  <Link href={`/blog/${content.id}`}>
-                    <Text fontSize={16} fontWeight={700}>
-                      {content.title}
-                    </Text>
-                  </Link>
-                  <Flex mt={"0.375rem"} fontSize={12}>
-                    {content.author}
+                <Flex mt={"0.375rem"} fontSize={12}>
+                  <Text mr={"0.375rem"}>{content.author}</Text>
+                  <Text>
                     {content.publishedAt &&
                       formatPublishedDate(content.publishedAt)}
-                  </Flex>
-                </Box>
-              </Flex>
-            </Box>
+                  </Text>
+                </Flex>
+              </Box>
+            </Flex>
           );
         })}
-      </HStack>
+      </Flex>
       {/* <p>{`記事の総数：${blogData.totalCount}件→もっと見る`}</p> */}
     </>
   );
